43ba605674a0edfa107ed0a08232ab6ba6006f56
bau7i <lonardibautista@gmail.com>
2022-05-09 18:26:57 -0300

    Arreglo de errror en script js.

--- segundaEntrega_pf/script.js
+++ segundaEntrega_pf/script.js
@@ -1,34 +1,82 @@
-const clickButton = document.querySelectorAll('.button');
-// console.log(clickButton);
-const tbody = document.querySelector('.tbody');
-let carrito = []
+const cards = document.getElementById('cards');
+const items = document.getElementById('items');
+const footer = document.getElementById('footer');
+const templateCard = document.getElementById('template-card').content;
+const templateFooter = document.getElementById('template-footer').content;
+const templateCarrito = document.getElementById('template-carrito').content;
 
-clickButton.forEach(btn => {
-    btn.addEventListener('click', addToCartProject)
+const fragment = document.createDocumentFragment()
+
+document.addEventListener('DOMContentLoaded', () => {
+    getProjectNames()
 })
 
-function addToCartProject(e){
-    const button = e.target;
-    // console.log(button)
-    // con closest tomo la carta mas cercana 
-    const item = button.closest('.card');
-    // console.log(item)
-    // y luego los valores de la carta mas cercana, la card entera
-    const itemTitle = item.querySelector('.card-title').textContent;
-    console.log(itemTitle);
-    const itemPrice = item.querySelector('.precio').textContent;
-    console.log(itemPrice)
-    const itemImg = item.querySelector('.card-img-top').src;
-    // console.log(itemImg)
-    //creo un objeto con los datos del item/proyecto
-    const newItem = {
-        title: itemTitle,
-        precio: itemPrice,
-        imagen: itemImg,
+//los items(cards), detectan el click e imprimimos lo que clickeamos
+cards.addEventListener('click', e => {
+    //  console.log(e);
+    addToCarrito(e)
+}) 
+
+const tbody = document.querySelector('.tbody'); 
+let carrito = []
+
+
+//Traigo la data del archivo local (API)
+const getProjectNames = async () => {
+    try{
+        const URL2 = 'fetch/data/data.json'
+        const res = await fetch(URL2)
+        const data = await res.json()
+        // console.log(data)
+        pintarCards(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+//Pinto la data de la API en cartas en el html
+const pintarCards = data => {
+    // console.log(data)
+    data.forEach(async (proyecto) => {
+        // console.log(proyecto)
+        templateCard.querySelector('.card-img-top').setAttribute("src", proyecto.img)
+        templateCard.querySelector('.card-title').textContent = proyecto.nombre
+        templateCard.querySelector('#category').textContent = proyecto.categoria
+        templateCard.querySelector('.card-text').textContent = proyecto.presentacion
+        templateCard.querySelector('.precio').textContent = proyecto.precio
+        templateCard.querySelector('.btn-primary').dataset.id = proyecto.id
+        // console.log(templateCard)
+        const clone = templateCard.cloneNode(true)
+        // console.log(clone)
+        fragment.appendChild(clone) 
+    })
+    cards.appendChild(fragment)
+}
+
+
+const addToCarrito = e => {
+        // console.log(e.target)
+        // console.log(e.target.classList.contains('btn-primary'));
+        if (e.target.classList.contains('btn-primary')) {
+            // console.log(e.target.parentElement);
+            setCarrito(e.target.parentElement)
+        }
+        e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+    console.log(objeto);
+    // // OBJETO CON DATOS DE CARTA CLICKEADA
+    const newItem2 = {
+        id: objeto.querySelector('.btn-primary').dataset.id,
+        title: objeto.querySelector('.card-title').textContent,
+        precio: objeto.querySelector('.precio').textContent,
+        imagen: objeto.querySelector('.card-img-top').src,
         cantidad: 1,
     }
-    // aniado funcion de aniadir item/proyectos al carrito[array]
-    addProjectCart(newItem)
+
+    console.log(newItem2)
+    addProjectCart(newItem2)
 }
 
 function addProjectCart(newItem){
@@ -41,14 +89,12 @@ function addProjectCart(newItem){
     alert.classList.remove('hide')
 
     const inputElemento = tbody.getElementsByClassName('input__elemento');
-    // console.log(inputElemento)
     
     for(let i = 0; i < carrito.length; i++){
         if(carrito[i].title.trim() === newItem.title.trim()){
             console.log(carrito[i].title)
             carrito[i].cantidad ++;  
             const inputValue = inputElemento[i];
-            console.log(inputValue)
             inputValue.value++;
             carritoTotal();
             return null;
@@ -61,29 +107,28 @@ function addProjectCart(newItem){
 }
 
 function renderCarrito(){
-    tbody.innerHTML = ''
     console.log(carrito);
-    carrito.map(item => {
+    Object.values(carrito).forEach(proyecto => {
         const tr = document.createElement('tr');
-        tr.classList.add('projectCart')
-        const content = `
-        
-        <th scope="row">1</th>
-            <td class="table__proyectos">
-                <img src="${item.imagen}" alt="">
-                <h6 class="title">${item.title}</h6>	
-            </td>
-            <td class="table__price"><p>${item.precio}</p></td>
-            <td class="table__cantidad">
-              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
-              <button class="delete btn btn-danger">x</button>
-            </td>
+            tr.classList.add('projectCart');
+            const content = `
+            
+            <th scope="row">1</th>
+                <td class="table__proyectos">
+                    <img src="${proyecto.imagen}" alt="">
+                    <h6 class="title">${proyecto.title}</h6>	
+                </td>
+                <td class="table__price"><p>${proyecto.precio}</p></td>
+                <td class="table__cantidad">
+                  <input type="number" min="1" value=${proyecto.cantidad} class="input__elemento">
+                  <button class="delete btn btn-danger">x</button>
+                </td>
             `
-        tr.innerHTML = content;
-        tbody.appendChild(tr);
-
-        tr.querySelector(".delete").addEventListener('click', removeProjectCart)
-        tr.querySelector('.input__elemento').addEventListener('change',sumaCantidad);
+            tr.innerHTML = content;
+            tbody.appendChild(tr);
+    
+            tr.querySelector(".delete").addEventListener('click', removeProjectCart)
+            tr.querySelector('.input__elemento').addEventListener('change',sumaCantidad);
     })
 
     carritoTotal();
@@ -140,20 +185,6 @@ function sumaCantidad(e){
 
 }
 
-// {/* <td class="table__categoria">
-                
-
-                // <button class="delete btn btn-danger">X</button>
-            // </td>        */}
-
-// <select class="form-select" name="categoria-proyecto-carrito" id="categoria-proyecto-carrito">
-                //     <option value="categoria-tecnologia">Tecnologia</option>
-                //     <option value="categoria-medioambiente">MedioAmbiente</option>
-                //     <option value="categoria-gastronomia">Gastronomia</option>
-                //     <option value="categoria-social">Social</option>	
-                // </select>
-
-
 function addLocalStorage(){
     localStorage.setItem('carrito', JSON.stringify(carrito))
 }
@@ -163,7 +194,6 @@ window.onload = function(){
     
     if(storage){
         carrito = storage
-        console.log(carrito)
         renderCarrito()
     }
 }
